bf3ab6ea98ec8fc16c9e6e4a8db08e83096aa8ab
mg0416 <songmingeon65@gmail.com>
2025-11-21 22:01:06 +0900

    Add files via upload

---
+++ triangle coding.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    int b;
+    for (  a=1;  a<=10; a++)
+    { for(b=1; b<=a; b++){
+
+    printf("*" );
+
+    }
+
+        printf("\n"  );
+    }
+    return 0;
+}
