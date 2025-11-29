56eec3d31c9d58963890520a32e4fc4bc723c896
mg0416 <songmingeon65@gmail.com>
2025-11-29 16:11:43 +0900

    Add files via upload

---
+++ greed.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int chan;
+  int a;
+  int b;
+  int c;
+  int d;
+  int e;
+  int f;
+  int g;
+  int h;
+  int i;
+  int j;
+  scanf("%d", &a);
+  scanf("%d", &b);
+  scanf("%d", &c);
+  scanf("%d", &d);
+  scanf("%d", &e);
+  scanf("%d", &f);
+  scanf("%d", &g);
+  scanf("%d", &h);
+  scanf("%d", &i);
+  scanf("%d", &j);
+  if(a<chan){
+
+  }
+  else {
+    chan=a;
+  }
+   if(b<chan){
+
+  }
+  else {
+    chan=b;
+  }  if(c<chan){
+
+  }
+  else {
+    chan=c;
+  }  if(d<chan){
+
+  }
+  else {
+    chan=d;
+  }  if(e<chan){
+
+  }
+  else {
+    chan=e;
+  }  if(f<chan){
+
+  }
+  else {
+    chan=f;
+  }if(g<chan){
+
+  }
+  else {
+    chan=g;
+  }if(h<chan){
+
+  }
+  else {
+    chan=h;
+  }if(i<chan){
+
+  }
+  else {
+    chan=i;
+  }if(j<chan){
+
+  }
+  else {
+    chan=j;
+  }
+  printf("%d",chan);
+
+
+
+
+
+
+    return 0;
+}
