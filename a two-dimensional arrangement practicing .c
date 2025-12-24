2faefbfcad1f85c16d62e50ab3cd209afd90acf0
mg0416 <songmingeon65@gmail.com>
2025-12-24 21:21:37 +0900

    Create a two-dimensional arrangement practicing .c

---
+++ a two-dimensional arrangement practicing .c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int a;
+   int arr[a][a];
+   scanf("%d",&a );
+   for(int m=0; m<a ; m++){
+    scanf("%d",& int h);
+       arr[m][0]=h;
+}
+   if( arr[a+1][a+2]=arr[a+1][j]-arr[a][a]){
+    printf("%d",)
+   }
+    
+     
+   
+   
+    return 0;
+}
