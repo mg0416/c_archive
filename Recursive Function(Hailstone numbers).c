8d662232392ced31efcf9b378bdd0e982268e066
mg0416 <songmingeon65@gmail.com>
2025-12-31 21:36:35 +0900

    Recursive Function(Hailstone numbers).c

---
+++ Recursive Function(Hailstone numbers).c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int function(int x){
+       int r;
+       if(r=1){
+            break;
+       }
+       if(x%2==1){
+        r= 3* return  function(x)+1;}
+          else if(x%2==0){
+            r=return  function(x)/2;
+                  }
+        printg("%d ", r );
+            }
+
+
+
+
+
+}
+
+int main()
+{
+    int j;
+   scanf("%d",&  j);
+   function(j);
+
+    return 0;
+}
