f79817ed089d27beca9f7c191fcf9862ce81596a
mg0416 <songmingeon65@gmail.com>
2026-02-21 18:23:49 +0900

    Update 12605.cpp

--- 12605.cpp
+++ 12605.cpp
@@ -7,23 +7,38 @@ using namespace std;
 int main(){
   int n;
   cin>> n;
+
+  cin.ignore();
   
   for(int a=1; a<=n; a++){
-    cout<< " case #" << a;
-    stack<char> s;
+    cout<< "Case #" << a<<":";
+    stack<string> s;
     string str;
     getline(cin, str);
-    while( sstream >> s){
-      for(int a=0; a< str.size(); a++){
-        s.push(str[a]);
+    
+    string temp;
+    
+    // "this" , "is", "a", "test"
+    stringstream ss(str);
+  
+      while(ss>> temp){
+        s.push(temp);
       }
-    }
-    while(!s.empty()){
-      cout<< s.top();
-      s.pop();
-    }
+      
+      
+      while(!s.empty()){
+        cout<<' ' << s.top();
+        s.pop();
+      }
+      cout<< '\n';
   }
-  //인덱스 접근 & 단어를 안바꾸며 변환;;
+  
+}
+   
+    
+    
+    
+     
   
   
   
